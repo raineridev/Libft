adce4c36df1ca43926884c377b41681a85ef4607
Marcelo RAineri <raineridev@gmail.com>
2024-04-22 14:37:22 +0100

    Adding Ft_Strstr files

---
+++ ft_strstr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mraineri <mraineri@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 18:24:15 by mraineri          #+#    #+#             */
+/*   Updated: 2024/04/22 14:37:01 by mraineri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while(haystack[i + j] == needle[j])
+		{
+			if(needle[j + 1] == '\0')
+				return &((char *)haystack)[i];
+			j++;
+		}
+		i++;
+	}	
+	return "NULL";
+}
+
+//int	main(void)
+//{
+//	char *str = "D tomate tomas arroz e carne";
+//	char *needle = "tomas";
+//	printf("1#'%s'\n", ft_strstr(str, needle));
+//	printf("2#'%s'\n", strstr(str, needle));
+//}
