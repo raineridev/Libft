ce79ee4af26184685ee6d3d7849b32e972a7189a
Marcelo Raineri <marcelo@gutlz.com>
2024-08-31 07:55:37 +0100

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -19,15 +19,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	if (!dest || !src)
 		return (NULL);
 	if (dest < src)
-	{
 		return (ft_memcpy(dest, src, n));
-	}
 	if (dest > src)
 	{
 		while (n--)
-		{
 			((char *)dest)[n] = ((char *)src)[n];
-		}
 	}
 	return (dest);
 }
